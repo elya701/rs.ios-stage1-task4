8ed6321f3ee713a7b87fbdc34f627f33a9f39289
elya701 <52858764+elya701@users.noreply.github.com>
2020-04-12 21:34:19 +0300

    fix: binary tree with even count of sides

--- RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.m
+++ RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.m
@@ -2,12 +2,13 @@
 
 @implementation FullBinaryTrees
 - (NSString *)stringForNodeCount:(NSInteger)count {
-    if (count == 0) {
+    if (count == 0 || count % 2 == 0) {
         return @"[]";
     } else if (count == 1) {
         return @"[[0]]";
     }
     return 0;
+    
 }
 
 @end
