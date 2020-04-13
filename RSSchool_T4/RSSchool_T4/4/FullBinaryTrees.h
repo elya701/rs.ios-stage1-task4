453311fbe923af8d97aa6eff3c3bd270726ca7ae
elya701 <52858764+elya701@users.noreply.github.com>
2020-04-13 22:07:26 +0300

    fix: fixed test 3-6 in FBT

--- RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.h
+++ RSSchool_T4/RSSchool_T4/4/FullBinaryTrees.h
@@ -1,6 +1,12 @@
 #import <Foundation/Foundation.h>
 
+@interface Node : NSObject
+@property (nonatomic, assign) NSInteger* val;
+@property (nonatomic, assign) Node *left;
+@property (nonatomic, assign) Node *right;
+- (instancetype)initWithValue:(NSInteger* )value;
+@end
+
 @interface FullBinaryTrees : NSObject
 - (NSString *)stringForNodeCount:(NSInteger)count;
-
 @end
